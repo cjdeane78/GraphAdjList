01a315307756ca4678cedf976a0fdfd225c68385
Chris Deane <cjdeane789@gmail.com>
2018-07-17 22:53:10 +0100

    Add project files.

---
+++ GraphAdjacencyList/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+    Class Graph
+        Property Edges As Dictionary(Of String, String())
+    End Class
+
+    Sub Main()
+        Dim Graph1 As New Graph
+
+        Graph1.Edges = New Dictionary(Of String, String()) From
+            {
+            {"A", {"B", "C"}},
+            {"B", {"A", "C", "D"}},
+            {"C", {"A", "B"}},
+            {"D", {"C"}}
+            }
+
+        For Each edge In Graph1.Edges("A")
+            Console.WriteLine(edge)
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ GraphAdjacencyList/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GraphAdjacencyList")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GraphAdjacencyList")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bc8eb877-5e4e-45de-ad97-9999bc6d47c0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
